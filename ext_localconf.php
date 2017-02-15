a07a80b1d83155b2d9181759cdb96fba006167e3
Benni Mack <benni@typo3.org>
2017-02-15 07:10:47 +0100

    [TASK] use PHP class syntax for ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,10 @@
 <?php
-
 defined('TYPO3_MODE') or die();
 
 // Define state cache, if not already defined
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['geocoding'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['geocoding'] = array(
-        'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
-        'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
-    );
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['geocoding'] = [
+        'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
+        'backend'  => \TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class,
+    ];
 }
