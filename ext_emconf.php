0ad81db446ef30099950c2ffe91ffa6cf1ec7e5e
Benni Mack <benni@typo3.org>
2017-02-15 07:12:35 +0100

    [TASK] Raise version to 2.0.0 and update requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,16 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "geocoding".
-#
-# Auto generated 27-09-2012 15:06
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+//#######################################################################
+// Extension Manager/Repository config file for ext "geocoding".
+//
+// Auto generated 27-09-2012 15:06
+//
+// Manual updates:
+// Only the data in the array - everything else is removed by next
+// writing. "version" and "dependencies" must not be touched!
+//#######################################################################
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Service: Geocoding via Google Maps',
     'description' => 'Provides services for google maps GeoCoding API and radius search on the database.',
     'category' => 'sv',
@@ -29,17 +29,17 @@ $EM_CONF[$_EXTKEY] = array(
     'clearCacheOnLoad' => 0,
     'lockType' => '',
     'author_company' => 'b:dreizehn GmbH',
-    'version' => '1.2.4',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '6.2.0-7.99.99',
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
-        ),
-    ),
+    'version' => '2.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '6.2.0-8.9.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
     '_md5_values_when_last_written' => 'a:19:{s:12:"ext_icon.gif";s:4:"7e66";s:14:"ext_tables.php";s:4:"886a";s:13:"mod1/conf.php";s:4:"0534";s:14:"mod1/index.php";s:4:"cd0f";s:18:"mod1/locallang.xml";s:4:"1701";s:22:"mod1/locallang_mod.xml";s:4:"ab3a";s:22:"mod1/mod_template.html";s:4:"7c59";s:19:"mod1/moduleicon.gif";s:4:"73cc";s:24:"templates/controller.php";s:4:"2f9e";s:30:"templates/controlleraction.php";s:4:"c426";s:36:"templates/controllerextlocalconf.php";s:4:"d15f";s:33:"templates/controllerexttables.php";s:4:"fa20";s:19:"templates/model.php";s:4:"6a17";s:25:"templates/modelgetset.php";s:4:"111d";s:38:"templates/modelgetsetobjectstorage.php";s:4:"0fec";s:27:"templates/modelvariable.php";s:4:"a3b7";s:24:"templates/repository.php";s:4:"e4f7";s:34:"templates/TypoScript/constants.txt";s:4:"7cf7";s:30:"templates/TypoScript/setup.txt";s:4:"4671";}',
-    'suggests' => array(
-    ),
-);
+    'suggests' => [
+    ],
+];
